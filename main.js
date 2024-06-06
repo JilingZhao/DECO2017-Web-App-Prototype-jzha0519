1b18a08f4230af65afd0da9cf99a4e50ef92cada
JilingZhao <jzha0519@uni.sydney.edu.au>
2024-06-06 16:30:34 +0800

    Create main.js
    The main content of the film information

---
+++ main.js
@@ -0,0 +1,84 @@
+const movies = [  // Define an array containing movie information
+  {
+    season: 1, // Number of seasons
+    title: "Harry Potter and the Sorcerer's Stone", // Movie title
+    img: "images/movie1.jpg", // Movie poster picture 
+    video: "videos/HP1.mp4", // Movies and videos
+    duration: "152:00", // Length of the movie
+    score: 7.6, // Movie rating
+    introduce: //Movie introduce
+      "The story follows an orphan named Harry Potter who discovers he is a wizard and is sent to study at Hogwarts School of Witchcraft and Wizardry. At Hogwarts, Harry makes friends and professors, and uncovers the secrets of his mysterious past. Together, they discover a powerful magical object known as the Philosopher's Stone, and Harry and his friends must stop it from falling into the hands of evil people.",
+  },
+  
+  // The following is a repetition of the movie content information
+  {
+    season: 2,
+    title: "Harry Potter and the Chamber of Secrets",
+    img: "images/movie2.jpg",
+    video: "videos/HP2.mp4",
+    duration: "161:00",
+    score: 7.4,
+    introduce:
+      "Harry Potter returns to Hogwarts School of Witchcraft and Wizardry to discover a mysterious space known as the Chamber of Secrets, which harbors terrifying creatures that could harm all non-pure-blood students. Harry, along with his friends Hermione and RON, embark on an adventure to decrypt and fight evil forces in order to keep the school and his friends safe.",
+  },
+  {
+    season: 3,
+    title: "Harry Potter and the Prisoner of Azkaban",
+    img: "images/movie3.jpg",
+    video: "videos/HP3.mp4",
+    duration: "142:00",
+    score: 7.9,
+    introduce:
+      "Harry learns that the dangerous fugitive Sirius Black has escaped from the wizarding prison Azkaban and may pose a threat to him. With the help of his friends Hermione and RON, Harry uncovers the secrets of Blake's true identity and past, as well as discovering more about his parents.",
+  },
+  {
+    season: 4,
+    title: "Harry Potter and the Goblet of Fire",
+    img: "images/movie4.jpg",
+    video: "videos/HP4.mp4",
+    duration: "157:00",
+    score: 7.7,
+    introduce:
+      "Harry Potter is accidentally chosen to take part in the dangerous Triwizard Tournament, taking on three other players in a series of challenges. As the game progresses, Harry not only has to face the threat of his life, but also uncovers the plot of Voldemort's resurrection, laying the groundwork for the subsequent magical war.",
+  },
+  {
+    season: 5,
+    title: "Harry Potter and the Order of the Phoenix",
+    img: "images/movie5.jpg",
+    video: "videos/HP5.mp4",
+    duration: "138:00",
+    score: 7.5,
+    introduce:
+      "Harry Potter discovers that the Ministry of Magic has denied Voldemort's return and is trying to cover it up, creating a secret organization called the Order of the Phoenix to fight against the dark forces. Meanwhile, Harry and his friends train in secret at Hogwarts in preparation for the coming Great War and end up in a fierce battle with the Death Eaters at the Ministry of Magic.",
+  },
+  {
+    season: 6,
+    title: "Harry Potter and the Half-Blood Prince",
+    img: "images/movie6.jpg",
+    video: "videos/HP6.mp4",
+    duration: "153:00",
+    score: 7.6,
+    introduce:
+      "Harry Potter discovers a mysterious potions textbook with useful magical techniques and gradually uncovers the identity of the Half-Blood Prince. Meanwhile, Headmaster Dumbledore and Harry explore Voldemort's past together, searching for the key to his destruction, the Horcrux, in preparation for the final showdown.",
+  },
+  {
+    season: 7,
+    title: "Harry Potter and the Deathly Hallows: Part 1",
+    img: "images/movie7-1.jpg",
+    video: "videos/HP7-1.mp4",
+    duration: "146:00",
+    score: 7.7,
+    introduce:
+      "Harry Potter and his friends Hermione and RON leave Hogwarts on a dangerous journey to find and destroy Lord Voldemort's Horcruxes. As they uncover more secrets, they face trials and betrayals, while Voldemort's power grows and threatens the wizarding world.",
+  },
+  {
+    season: 7,
+    title: "Harry Potter and the Deathly Hallows: Part 2",
+    img: "images/movie7-2.jpg",
+    video: "videos/HP7-2.mp4",
+    duration: "130:00",
+    score: 8.1,
+    introduce:
+      "Harry Potter and his friends continue to search for and destroy Voldemort's last Horcruxes, culminating in the decisive final battle at Hogwarts. Harry's final showdown with Lord Voldemort reveals the hidden truth that determines the fate of the wizarding world.",
+  },
+];
